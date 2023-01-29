6b8a38c43bce0cd5b3e5b4582e43495278b6cf35
Ethan Hofton <ethanhofton@gmail.com>
2023-01-29 15:51:40 +0000

    filled out GLFW window API functions (with support for OPENGL), created application interface between application and windowAPI, linked GLFW in CMakeLists.txt

--- include/ere/impl/glfwWindowImpl.hpp
+++ include/ere/impl/glfwWindowImpl.hpp
@@ -6,6 +6,14 @@
 
 #if defined(USE_GLFW)
 
+#ifdef USE_OPENGL
+// * include glad.h BEFORE glfw3.h
+#include <glad/glad.h>
+#endif
+
+#include <GLFW/glfw3.h>
+#include <ere/core/logger.hpp>
+
 namespace ere {
 
 class glfwWindowImpl : public windowAPI {
@@ -16,12 +24,20 @@ class glfwWindowImpl : public windowAPI {
     virtual void setWindowPos(const glm::vec2& t_pos);
     virtual void setWindowTitle(const std::string& t_title);
     virtual void setWindowSwapInterval(const int& t_fps);
-    virtual glm::vec2 getWindowSize();
-    virtual glm::vec2 getWindowPos();
+    virtual glm::vec2 getWindowSize() const;
+    virtual glm::vec2 getWindowPos() const;
     virtual void maximizeWindow();
     virtual void minimizeWindow();
     virtual void restoreWindow();
     virtual void focusWindow();
+    virtual void preRender();
+    virtual void postRender();
+    virtual void setBackgroundColor(const glm::vec4& t_color);
+
+private:
+
+    GLFWwindow *m_window = nullptr;
+    glm::vec4 m_backgroundColor = {1.0f, 1.0f, 1.0f, 1.0f};
 };
 
 }
--- src/glfwWindowImpl.cpp
+++ src/glfwWindowImpl.cpp
@@ -9,19 +9,87 @@ std::shared_ptr<windowAPI> windowAPI::createConcreteWindowAPI() {
     return std::make_shared<glfwWindowImpl>();
 }
 
-void glfwWindowImpl::createWindow(const windowProps& t_props) {}
-void glfwWindowImpl::destroyWindow() {}
+void glfwWindowImpl::createWindow(const windowProps& t_props) {
+    /* -- GLFW setup -- */
+    ERE_INFO("Initalizing GLFW");
+    if (!glfwInit()) {
+        ERE_CRITICAL("GLFW initalization failed");
+        throw std::runtime_error("glfw initalization failed");
+    }
 
-void glfwWindowImpl::setWindowSize(const glm::vec2& t_size) {}
-void glfwWindowImpl::setWindowPos(const glm::vec2& t_pos) {}
-void glfwWindowImpl::setWindowTitle(const std::string& t_title) {}
-void glfwWindowImpl::setWindowSwapInterval(const int& t_fps) {}
-glm::vec2 glfwWindowImpl::getWindowSize() {}
-glm::vec2 glfwWindowImpl::getWindowPos() {}
-void glfwWindowImpl::maximizeWindow() {}
-void glfwWindowImpl::minimizeWindow() {}
-void glfwWindowImpl::restoreWindow() {}
-void glfwWindowImpl::focusWindow() {}
+    #ifdef USE_OPENGL
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    #endif
+    
+    ERE_INFO("Creating window...");
+    m_window = glfwCreateWindow(t_props.m_width, t_props.m_height, t_props.m_title.c_str(), NULL, NULL);
+
+    if (m_window == nullptr) {
+        glfwTerminate();
+        ERE_CRITICAL("Failed to create window");
+        throw std::runtime_error("Failed to create window");
+    }
+
+    glfwMakeContextCurrent(m_window);
+
+    #ifdef USE_OPENGL
+    // * load glad
+    gladLoadGL(glfwGetProcAddress);
+
+    // * set viewport 
+    glViewport(0, 0, t_props.t_width, t_props.t_height);
+    #endif
+
+    /* -- GLFW callbacks -- */
+
+    // * glfw framebuffer size callback
+    glfwSetFramebufferSizeCallback(m_window, [](GLFWwindow* t_window, int t_w, int t_h) {
+        #ifdef USE_OPENGL
+        glViewport(0, 0, t_w, t_h);
+        #endif
+    });
+
+    // * glfw error callback
+    glfwSetErrorCallback([](int t_code, const char* t_msg) {
+        ERE_ERROR("GLFW error ({}) with message: {}", t_code, t_msg);
+    });
+}
+
+void glfwWindowImpl::destroyWindow() {
+    ERE_INFO("Destroying window...");
+    glfwDestroyWindow(m_window);
+    ERE_INFO("Terminating GLFW");
+    glfwTerminate();
+}
+
+void glfwWindowImpl::setWindowSize(const glm::vec2& t_size) { glfwSetWindowSize(m_window, t_size.x, t_size.y); }
+void glfwWindowImpl::setWindowPos(const glm::vec2& t_pos) { glfwSetWindowPos(m_window, t_pos.x, t_pos.y); }
+void glfwWindowImpl::setWindowTitle(const std::string& t_title) { glfwSetWindowTitle(m_window, t_title.c_str()); }
+void glfwWindowImpl::setWindowSwapInterval(const int& t_fps) { glfwSwapInterval(t_fps); }
+glm::vec2 glfwWindowImpl::getWindowSize() const {
+    glm::ivec2 size;
+    glfwGetWindowSize(m_window, &size.x, &size.y);
+    return size;
+}
+glm::vec2 glfwWindowImpl::getWindowPos() const {
+    glm::ivec2 size;
+    glfwGetWindowPos(m_window, &size.x, &size.y);
+    return size;
+}
+void glfwWindowImpl::maximizeWindow() { glfwMaximizeWindow(m_window); }
+void glfwWindowImpl::minimizeWindow() { glfwIconifyWindow(m_window); }
+void glfwWindowImpl::restoreWindow() { glfwRestoreWindow(m_window); }
+void glfwWindowImpl::focusWindow() { glfwFocusWindow(m_window); }
+void glfwWindowImpl::preRender() {
+    #ifdef USE_OPENGL
+    glClearColor(m_backgroundColor.r, m_backgroundColor.g, m_backgroundColor.b, m_backgroundColor.a);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    #endif
+}
+void glfwWindowImpl::postRender() { glfwSwapBuffers(m_window); glfwPollEvents(); }
+void glfwWindowImpl::setBackgroundColor(const glm::vec4& t_color) { m_backgroundColor = t_color; }
 
 }
 
