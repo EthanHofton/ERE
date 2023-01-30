92c464fb425044fbe25c3bc0e5be7cf3501dfa7a
ethan hofton <ethanhofton@gmail.com>
2023-01-30 01:11:49 +0000

    changed function is layer.hpp to have correct signitures (changed void to bool) and defined on event function using util::event_dispatcher

--- include/ere/core/layer.hpp
+++ include/ere/core/layer.hpp
@@ -15,30 +15,30 @@ public:
     virtual void onEvent(ereEvent& t_event) override final;
 
     /* -- application events -- */
-    inline virtual void onUpdate(updateEvent& t_event) {}
-    inline virtual void onAttach(attachEvent& t_event) {}
-    inline virtual void onDetach(detachEvent& t_event) {}
+    inline virtual bool onUpdate(updateEvent& t_event) {}
+    inline virtual bool onAttach(attachEvent& t_event) {}
+    inline virtual bool onDetach(detachEvent& t_event) {}
 
     /* -- window events -- */
-    inline virtual void onWindowClose(windowClosedEvent& t_event) {}
-    inline virtual void onWindowMoved(windowMovedEvent& t_event) {}
-    inline virtual void onWindowResize(windowResizeEvent& t_event) {}
-    inline virtual void onWindowFocus(windowFocusEvent& t_event) {}
-    inline virtual void onWindowLostFocus(windowLostFocusEvent& t_event) {}
-    inline virtual void onWindowMinimized(windowMinimizedEvent& t_event) {}
-    inline virtual void onWindowMaximised(windowMaximisedEvent& t_event) {}
-    inline virtual void onWindowRestored(windowRestoredEvent& t_event) {}
+    inline virtual bool onWindowClose(windowClosedEvent& t_event) {}
+    inline virtual bool onWindowMoved(windowMovedEvent& t_event) {}
+    inline virtual bool onWindowResize(windowResizeEvent& t_event) {}
+    inline virtual bool onWindowFocus(windowFocusEvent& t_event) {}
+    inline virtual bool onWindowLostFocus(windowLostFocusEvent& t_event) {}
+    inline virtual bool onWindowMinimized(windowMinimizedEvent& t_event) {}
+    inline virtual bool onWindowMaximised(windowMaximisedEvent& t_event) {}
+    inline virtual bool onWindowRestored(windowRestoredEvent& t_event) {}
 
     /* -- key events -- */
-    inline virtual void onKeyPressed(keyPressedEvent &t_event) {}
-    inline virtual void onKeyReleased(keyReleasedEvent &t_event) {}
-    inline virtual void onKeyTyped(keyTypedEvent &t_event) {}
+    inline virtual bool onKeyPressed(keyPressedEvent &t_event) {}
+    inline virtual bool onKeyReleased(keyReleasedEvent &t_event) {}
+    inline virtual bool onKeyTyped(keyTypedEvent &t_event) {}
 
     /* -- mouse events -- */
-    inline virtual void onMouseButtonPressed(mouseButtonPressedEvent &t_event) {}
-    inline virtual void onMouseButtonReleased(mouseButtonReleasedEvent &t_event) {}
-    inline virtual void onMouseMoved(mouseMovedEvent &t_event) {}
-    inline virtual void onMouseScrolled(mouseScrolledEvent &t_event) {}
+    inline virtual bool onMouseButtonPressed(mouseButtonPressedEvent &t_event) {}
+    inline virtual bool onMouseButtonReleased(mouseButtonReleasedEvent &t_event) {}
+    inline virtual bool onMouseMoved(mouseMovedEvent &t_event) {}
+    inline virtual bool onMouseScrolled(mouseScrolledEvent &t_event) {}
 };
 
 }
