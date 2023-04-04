c0ea0b140b6fbfcd2e3eaee9a57262e9a26ca6e1
ethan hofton <ethanhofton@gmail.com>
2023-04-04 14:51:20 +0100

    added push, pop layers and overlays to application, propagated events to layers on application event

--- include/ere/core/application.hpp
+++ include/ere/core/application.hpp
@@ -4,6 +4,9 @@
 #include <util/timer.hpp>
 #include <ere/events/events.hpp>
 #include <ere/events/window_events.hpp>
+#include <ere/events/app_events.hpp>
+
+#include "i_layer.hpp"
 
 #include "logger.hpp"
 #include "core.hpp"
@@ -45,9 +48,38 @@ public:
     * @param fps the fps
     */
     inline void set_fps(double fps) { m_fps = fps; }
+
+    /* -- layer stack -- */
+    /**
+    * @brief push a layer to the layer stack
+    *
+    * @param layer the layer
+    */
+    void push_layer(ref<i_layer> t_layer);
+    /**
+    * @brief push an overlay to the layer stack
+    *
+    * @param overlay the overlay
+    */
+    void push_overlay(ref<i_layer> t_overlay);
+    /**
+    * @brief pop a layer from the layer stack
+    *
+    * @param layer the layer
+    */
+    void pop_layer(ref<i_layer> t_layer);
+    /**
+    * @brief pop an overlay from the layer stack
+    *
+    * @param overlay the overlay
+    */
+    void pop_overlay(ref<i_layer> t_overlay);
     
 
 private:
+
+    std::vector<ref<i_layer>> m_layers;
+    unsigned int m_layersEnd = 0;
 
     double m_fps = 60.0;
     bool m_running = true;
