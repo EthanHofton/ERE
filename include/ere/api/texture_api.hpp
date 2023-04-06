077bc619572e42710961f73a164e6de22b96670a
ethan hofton <ethanhofton@gmail.com>
2023-04-07 00:21:51 +0100

    Added stb_image, texture api, opengl texture api implimentation, render draw textured

---
+++ include/ere/api/texture_api.hpp
@@ -0,0 +1,41 @@
+#ifndef __ERE_TEXTURE_API_HPP__
+#define __ERE_TEXTURE_API_HPP__
+
+#include <ere/core/core.hpp>
+#include <string>
+
+namespace ere {
+
+class texture_api {
+public:
+
+    virtual ~texture_api() = default;
+
+    virtual void bind(uint32_t slot) const = 0;
+    virtual void unbind() const = 0;
+
+    virtual void set_data(unsigned char* data, size_t size, uint32_t width, uint32_t height) = 0;
+
+    inline void set_uniform_name(const std::string& name) { m_uniform_name = name; }
+    inline const std::string& get_uniform_name() const { return m_uniform_name; }
+
+    inline uint32_t get_width() const { return m_width; }
+    inline uint32_t get_height() const { return m_height; }
+    inline uint32_t get_depth() const { return m_depth; }
+
+    ref<texture_api> create_texture_api(const std::string& path);
+    ref<texture_api> create_texture_api(size_t size, uint32_t width, uint32_t height);
+    ref<texture_api> create_texture_api(unsigned char* data, size_t size, uint32_t width, uint32_t height);
+
+protected:
+
+    std::string m_uniform_name;
+    uint32_t m_width;
+    uint32_t m_height;
+    uint32_t m_depth;
+
+};
+
+}
+
+#endif
---
+++ include/ere/impl/opengl/opengl_texture.hpp
@@ -0,0 +1,36 @@
+#ifndef __ERE_OPENGL_TEXTURE_HPP__
+#define __ERE_OPENGL_TEXTURE_HPP__
+
+#include <ere/core/core.hpp>
+#include <ere/ere_config.hpp>
+#include <ere/api/texture_api.hpp>
+
+#ifdef USE_OPENGL
+
+namespace ere {
+
+class opengl_texture : public texture_api {
+public:
+
+    opengl_texture(const std::string& path);
+    opengl_texture(size_t size, uint32_t width, uint32_t height);
+    opengl_texture(unsigned char* data, size_t size, uint32_t width, uint32_t height);
+    ~opengl_texture() override;
+
+    void bind(uint32_t slot) const override;
+    void unbind() const override;
+
+    void set_data(unsigned char* data, size_t size, uint32_t width, uint32_t height) override;
+
+private:
+
+    unsigned int m_texture_id;
+
+};
+
+}
+
+#endif // USE_OPENGL
+
+
+#endif // __ERE_OPENGL_TEXTURE_HPP__
