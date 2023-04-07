2649880a1620cbc08921415d07984b9ffdbeba02
ethan hofton <ethanhofton@gmail.com>
2023-04-07 12:07:04 +0100

    updated tests

--- test/assets/shaders/vertex.glsl
+++ test/assets/shaders/vertex.glsl
@@ -4,11 +4,13 @@ layout (location = 0) in vec3 aPos;
 layout (location = 1) in vec3 aColor;
 layout (location = 2) in vec2 aUv;
 
+uniform mat4 u_projection;
+
 out vec3 outColor;
 out vec2 outUv;
 
 void main() {
-    gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);
+    gl_Position = u_projection * vec4(aPos, 1.0);
     outColor = aColor;
     outUv = aUv;
 }
