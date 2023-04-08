1973f4f7a14afcd3416b3e2ee0938f0692a5cee9
ethan hofton <ethanhofton@gmail.com>
2023-04-08 16:36:09 +0100

    added sponza scene and backback model for testing. experimented with model loading in test file. fixed small bug in opengl texture api for unbind

--- test/assets/shaders/fragment.glsl
+++ test/assets/shaders/fragment.glsl
@@ -1,13 +1,12 @@
 #version 410 core
 
-in vec3 outColor;
-in vec2 outUv;
-
-uniform sampler2D uContainer;
-uniform float u_time;
-
 out vec4 FragColor;
 
-void main() {
-    FragColor = vec4(vec3(texture(uContainer, outUv)) * outColor * sin(u_time), 1.);
+in vec2 TexCoords;
+
+uniform sampler2D texture_diffuse;
+
+void main()
+{    
+    FragColor = texture(texture_diffuse, TexCoords);
 }
--- test/assets/shaders/vertex.glsl
+++ test/assets/shaders/vertex.glsl
@@ -1,17 +1,15 @@
 #version 410 core
 
 layout (location = 0) in vec3 aPos;
-layout (location = 1) in vec3 aColor;
-layout (location = 2) in vec2 aUv;
+layout (location = 1) in vec2 aTexCoords;
+layout (location = 2) in vec2 aNormals;
 
 uniform mat4 u_projection;
 uniform mat4 u_view;
 
-out vec3 outColor;
-out vec2 outUv;
+out vec2 TexCoords;
 
 void main() {
+    TexCoords = aTexCoords;    
     gl_Position = u_projection * u_view * vec4(aPos, 1.0);
-    outColor = aColor;
-    outUv = aUv;
 }
